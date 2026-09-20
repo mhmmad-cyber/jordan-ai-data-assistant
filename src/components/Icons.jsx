62412feddd460e54caf8411f772cf101fef83ddc
Mohammed <mhmmadalsamer3@gmail.com>
2026-09-20 23:48:06 +0300

    Prepare project for GitHub

---
+++ src/components/Icons.jsx
@@ -0,0 +1,2 @@
+import { ArrowRight, BarChart3, Bot, Boxes, BrainCircuit, CheckCircle2, ChevronRight, Database, Menu, Sparkles, X } from 'lucide-react';
+export { ArrowRight, BarChart3, Bot, Boxes, BrainCircuit, CheckCircle2, ChevronRight, Database, Menu, Sparkles, X };
